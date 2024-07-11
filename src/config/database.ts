74b5a69cda7040856ebd273f00c51b792990ad4c
erisdll <72031082+erisdll@users.noreply.github.com>
2024-07-11 16:16:04 -0300

    update database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,11 @@
 import {Sequelize} from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize({
+    database: 'syntasker',
+    username: 'pguser',
+    password: 'pgpass',
+    host: 'localhost',
     dialect: 'postgres',
+    port: 5432
 })
-
 export default sequelize;
