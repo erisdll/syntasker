2700f2847f4fa02355fc04f4c97125fa8f651b68
erisdll <72031082+erisdll@users.noreply.github.com>
2024-07-11 16:15:30 -0300

    update main app

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
-import express, {Request, Response, NextFunction } from "express";
+import express, {Request, Response, NextFunction, Express} from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import authRoutes from './routes/auth';
-// import projectRoutes from './routes/projects';
-// import taskRoutes from './routes/tasks';
+import authRoutes from './routes/authRoutes';
+import projectRoutes from './routes/projectRoutes';
+import taskRoutes from './routes/taskRoutes';
+import * as fs from "node:fs";
 
 dotenv.config();
 
-const app = express();
+const app : Express = express();
 app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
-// app.use('/api/projects', projectRoutes);
-// app.use('/api/tasks', taskRoutes);
+app.use('/api/', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send({'error': 'Internal Server Error'});
+app.use((error: Error, request: Request, response: Response, next: NextFunction): void => {
+    console.error(error.stack);
+    response.status(500).send({'error': 'Internal Server Error'});
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port : string | number = process.env.PORT || 3000;
+app.listen(port, () : void => {
     console.log(`Listening on port ${port}`);
 });
