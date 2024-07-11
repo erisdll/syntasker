d1d128b10363748d8c6689d095c0f5acf9e04b35
erisdll <72031082+erisdll@users.noreply.github.com>
2024-07-11 10:31:35 -0300

    updated AuthController.ts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,24 +1,52 @@
-import {NextFunction, Request, Response} from "express";
+import {Request, Response} from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User"
 
 class AuthController {
-    static async register(req: Request, res: Response, next: NextFunction ) {
+    static async register(req: Request, res: Response) {
         try {
-            const { username, email, password, role } = req.body;
-            const hashedPassword = await bcrypt.hash(password, 12);
-            const user = await User.create({username, email, password: hashedPassword, role});
-            return res.status(201).send({ user });
+            const {username, email, password, role} = req.body;
+            const hashedPassword: string = await bcrypt.hash(password, 12);
+            const user: User = await User.create(
+                {username, email, password: hashedPassword, role}
+            );
+
+            return res.status(201).json({user});
         } catch (error) {
-            return res.status(500).send({ error});
+            return res.status(500).send({error});
         }
     }
 
-    static async login(req: Request, res: Response) {}
+    static async login(req: Request, res: Response) {
+        try {
+            const {username, email, password} = req.body;
+            const user: User | null = await User.findOne({where: username || email});
+
+            if (!user) {
+                return res.status(400).json({error: "User not found"});
+            }
+
+            const isMatch: boolean = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({error: "Username of password is incorrect"});
+            }
+
+            const token: string = jwt.sign(
+                {id: user.id, role: user.role},
+                process.env.JWT_SECRET!,
+                {expiresIn: '1h'}
+            );
+            return res.status(200).json({token});
+
+        } catch (error) {
+            return res.status(500).send({error});
+        }
+    }
 
     static logout(req: Request, res: Response) {
-        res.json({message: 'Logged out successfully'});
+        res.json({message: "Logged out successfully"});
     }
 }
 
